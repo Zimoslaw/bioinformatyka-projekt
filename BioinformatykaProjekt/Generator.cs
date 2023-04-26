3a4b12f6ea314414c29f2060f343f13b5601b5a9
Jakub <35482785+Zimoslaw@users.noreply.github.com>
2023-04-26 23:25:00 +0200

    Init

---
+++ BioinformatykaProjekt/Generator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioinformatykaProjekt
+{
+    public class Generator
+    {
+        private char[] Nucleobases =
+        {
+            'A', 'C', 'G', 'T'
+        };
+
+        public char[] Sequence;
+        public List<string> Spectrum;
+
+        public void Generate(uint dnaSize, uint oligoSize)
+        {
+            Sequence = new char[dnaSize];
+
+            Random random = new Random(DateTime.Now.Millisecond);
+
+            for (uint n = 0; n < dnaSize; n++)
+            {
+                Sequence[n] = Nucleobases[random.Next(0, 4)];
+            }
+
+            Spectrum = new List<string>();
+
+            for (uint i = 0; i <= dnaSize - oligoSize; i++)
+            {
+                string o = "";
+                for (uint j = 0; j < oligoSize; j++)
+                    o += (Sequence[i + j]);
+                if (!Spectrum.Contains(o))
+                    Spectrum.Add(o);
+            }
+        }
+    }
+}
---
+++ BioinformatykaProjekt/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using BioinformatykaProjekt;
+using System.Diagnostics;
+
+Generator gen1= new Generator();
+
+gen1.Generate(20, 6);
+
+Console.WriteLine("Sekwencja:");
+
+foreach (char n in gen1.Sequence)
+{
+    Console.Write(n);
+}
+
+Console.WriteLine("\nSpektrum:");
+
+foreach (string o in gen1.Spectrum)
+{
+    Console.Write($"[{o}],");
+}
